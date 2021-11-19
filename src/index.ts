d7fe6ed7bbc56847230b1d0ff283b1cae60262c3
Yerin Adler <yerin.adler@gmail.com>
2021-11-20 00:35:41 +0700

    Add convention on file names

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ import 'reflect-metadata';
 import { TYPES } from '@constants/types';
 import { EventHandler } from '@infrastructure/eventHandler';
 
-import { initialise } from './entrypoint';
+import { initialise } from './startup';
 
 (async () => {
   const container = await initialise();
