dc1fcd331a3cf86c8f462c0ee9bce83a6dcf968d
Yerin Adler <yerin.adler@gmail.com>
2022-04-07 10:41:24 +0700

    Revise error handler

--- src/interfaces/http/middlewares/ErrorHandler.ts
+++ src/interfaces/http/middlewares/ErrorHandler.ts
@@ -2,7 +2,8 @@ import { Request, Response, NextFunction } from 'express';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const errorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
-  return res.status(err.httpCode).json({
+  console.log(err);
+  return res.status(err.httpCode || 500).json({
     status: err.statusCode || '500',
     message: err.message,
   });
