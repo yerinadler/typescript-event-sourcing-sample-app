a4766c4e3df3d982a90942bf19139038933eef26
Yerin Adler <yerin.adler@gmail.com>
2022-05-05 13:19:45 +0700

    Remove onActivation()

--- src/common/module.ts
+++ src/common/module.ts
@@ -4,14 +4,11 @@ import { Kafka, Producer, Consumer } from 'kafkajs';
 import { Db } from 'mongodb';
 
 import config from '@config/main';
-import { ICommand } from '@core/ICommand';
 import { ICommandBus } from '@core/ICommandBus';
-import { ICommandHandler } from '@core/ICommandHandler';
 import { IEventBus } from '@core/IEventBus';
 import { IEventStore } from '@core/IEventStore';
 import { IQuery } from '@core/IQuery';
 import { IQueryBus } from '@core/IQueryBus';
-import { IQueryHandler } from '@core/IQueryHandler';
 import { JobEventStore } from '@src/job/infrastructure/event-store/job-event-store';
 
 import { CommandBus } from './infrastructure/commandBus';
@@ -36,43 +33,18 @@ export const commonContainerModule = new AsyncContainerModule(async (bind: inter
     await kafkaConsumer.subscribe({ topic });
   }
 
-  // Prepare Messaging Engine
   bind<Producer>(BASE_TYPES.KafkaProducer).toConstantValue(kafkaProducer);
   bind<Consumer>(BASE_TYPES.KafkaConsumer).toConstantValue(kafkaConsumer);
 
-  // Initialise Redis
   const redisSubscriber: Redis = getRedisClient();
   const redis: Redis = getRedisClient();
 
   bind<Redis>(BASE_TYPES.RedisSubscriber).toConstantValue(redisSubscriber);
   bind<Redis>(BASE_TYPES.Redis).toConstantValue(redis);
   bind<IEventBus>(BASE_TYPES.EventBus).to(KafkaEventBus);
-
-  // Prepare persistence components
   bind<Db>(BASE_TYPES.Db).toConstantValue(db);
-
   bind<IEventStore>(BASE_TYPES.EventStore).to(ApplicationEventStore).whenTargetNamed(EVENT_STREAM_NAMES.Application);
   bind<IEventStore>(BASE_TYPES.EventStore).to(JobEventStore).whenTargetNamed(EVENT_STREAM_NAMES.Job);
-
-  bind<ICommandBus>(BASE_TYPES.CommandBus)
-    .toConstantValue(new CommandBus())
-    .onActivation((context: interfaces.Context, commandBus) => {
-      context.container
-        .getAll<ICommandHandler<ICommand>>(BASE_TYPES.CommandHandler)
-        .forEach((handler: ICommandHandler<ICommand>) => {
-          commandBus.registerHandler(handler);
-        });
-      return commandBus;
-    });
-
-  bind<IQueryBus<IQuery>>(BASE_TYPES.QueryBus)
-    .toConstantValue(new QueryBus())
-    .onActivation((context: interfaces.Context, queryBus) => {
-      context.container
-        .getAll<IQueryHandler<IQuery>>(BASE_TYPES.QueryHandler)
-        .forEach((handler: IQueryHandler<IQuery>) => {
-          queryBus.registerHandler(handler);
-        });
-      return queryBus;
-    });
+  bind<ICommandBus>(BASE_TYPES.CommandBus).toConstantValue(new CommandBus());
+  bind<IQueryBus<IQuery>>(BASE_TYPES.QueryBus).toConstantValue(new QueryBus());
 });
