4aa281b286b1a2a1598e968fb6877a123097c326
Yerin Adler <yerin.adler@gmail.com>
2022-05-05 13:19:45 +0700

    Remove onActivation()

--- src/startup.ts
+++ src/startup.ts
@@ -9,12 +9,32 @@ import { applicationContainerModule } from '@src/application/module';
 import { commonContainerModule } from '@src/common/module';
 import { jobContainerModule } from '@src/job/module';
 
+import { BASE_TYPES } from './common/types';
+import { ICommand } from './core/ICommand';
+import { ICommandBus } from './core/ICommandBus';
+import { ICommandHandler } from './core/ICommandHandler';
+import { IQuery } from './core/IQuery';
+import { IQueryBus } from './core/IQueryBus';
+import { IQueryHandler } from './core/IQueryHandler';
+
 const initialise = async () => {
   const container = new Container();
 
   await container.loadAsync(commonContainerModule);
   container.load(jobContainerModule);
   container.load(applicationContainerModule);
+
+  const commandBus = container.get<ICommandBus>(BASE_TYPES.CommandBus);
+  container
+    .getAll<ICommandHandler<ICommand>>(BASE_TYPES.CommandHandler)
+    .forEach((handler: ICommandHandler<ICommand>) => {
+      commandBus.registerHandler(handler);
+    });
+
+  const queryBus = container.get<IQueryBus>(BASE_TYPES.QueryBus);
+  container.getAll<IQueryHandler<IQuery>>(BASE_TYPES.QueryHandler).forEach((handler: IQueryHandler<IQuery>) => {
+    queryBus.registerHandler(handler);
+  });
 
   const server = new InversifyExpressServer(container);
 
