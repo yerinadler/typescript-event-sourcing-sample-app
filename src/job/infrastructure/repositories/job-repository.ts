989c6d2a379f0eb3003fc779b79b28e37574668a
Yerin Adler <yerin.adler@gmail.com>
2022-05-05 09:51:55 +0700

    WIP: Restructuring

--- src/job/infrastructure/repositories/job-repository.ts
+++ src/job/infrastructure/repositories/job-repository.ts
@@ -5,7 +5,7 @@ import { IEventStore } from '@core/IEventStore';
 import { Job } from '@domain/job/job';
 import { IJobRepository } from '@domain/job/job-repository.interface';
 
-import { Repository } from './repository';
+import { Repository } from '../../../infrastructure/repositories/repository';
 
 @injectable()
 export class JobRepository extends Repository<Job> implements IJobRepository {
