cab46abcb736a098dfc4a5900b66ea6f0c86ab7d
Yerin Adler <yerin.adler@gmail.com>
2022-10-08 23:34:02 +0700

    feat: add event processing

--- packages/core/src/EventStore.ts
+++ packages/core/src/EventStore.ts
@@ -1,3 +1,5 @@
+import { instanceToPlain, plainToInstance } from 'class-transformer';
+import { EVENT_METADATA, EVENT_METADATA_TYPES } from './Event';
 import { injectable, unmanaged } from 'inversify';
 import { Collection } from 'mongodb';
 
@@ -6,6 +8,9 @@ import { EventDescriptor } from './EventDescriptor';
 import { IEvent } from './interfaces/IEvent';
 import { IEventBus } from './interfaces/IEventBus';
 import { IEventStore } from './interfaces/IEventStore';
+
+export type StorageEvent = Omit<IEvent, EVENT_METADATA_TYPES>;
+export class RehydratedEvent {}
 
 @injectable()
 export abstract class EventStore implements IEventStore {
@@ -28,9 +33,9 @@ export abstract class EventStore implements IEventStore {
     for (const event of events) {
       i++;
       event.version = i;
-      const eventObject = new EventDescriptor(aggregateGuid, event.aggregateName, event, i);
+      const eventDescriptor = this.createEventDescriptor(event);
       this._eventBus.publish(event.aggregateName, event);
-      operations.push({ insertOne: eventObject });
+      operations.push({ insertOne: eventDescriptor });
     }
 
     await this.eventCollection.bulkWrite(operations);
@@ -41,11 +46,34 @@ export abstract class EventStore implements IEventStore {
     if (!events.length) {
       throw new NotFoundException('Aggregate with the requested Guid does not exist');
     }
-    return events.map((eventDescriptor: EventDescriptor) => eventDescriptor.payload);
+    return events.map((eventDescriptor: EventDescriptor) => this.rehydrateEventFromStorage(eventDescriptor));
   }
 
   private async getLastEventDescriptor(aggregateGuid: string) {
     const [latestEvent] = await this.eventCollection.find({ aggregateGuid }, { sort: { _id: -1 } }).toArray();
     return latestEvent;
+  }
+
+  private createEventDescriptor<T extends IEvent = IEvent>(
+    event: T
+  ): EventDescriptor {
+    const JSONEvent = instanceToPlain(event);
+
+    for (const attribute of EVENT_METADATA) {
+      delete JSONEvent[attribute];
+    }
+
+    return new EventDescriptor(event.aggregateId, event.aggregateName, event.eventName, JSONEvent, event.version!);
+  }
+
+  private rehydrateEventFromStorage(storageEvent: EventDescriptor): IEvent {
+    const event: any = plainToInstance(RehydratedEvent, storageEvent);
+    return {
+      aggregateId: storageEvent.aggregateGuid,
+      aggregateName: storageEvent.aggregateName,
+      eventName: storageEvent.eventName,
+      version: storageEvent.version,
+      ...event.payload,
+    }
   }
 }
