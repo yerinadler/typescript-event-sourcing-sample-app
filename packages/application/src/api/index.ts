4fabb19b7f1d2e301a2215107f7bfd53b4c97b6f
Yerin Adler <yerin.adler@gmail.com>
2023-02-25 23:31:16 +0700

    feat: split subscription service

--- packages/application/src/api/index.ts
+++ packages/application/src/api/index.ts
@@ -7,10 +7,15 @@ import 'reflect-metadata';
 import { initialise } from '../startup';
 import config from '@config/main';
 import { TYPES } from '../types';
+import { Producer } from 'kafkajs';
 
 (async () => {
   const container = await initialise();
   const api: Application = container.get<Application>(TYPES.ApiServer);
+  
+  const kafkaProducer = container.get<Producer>(TYPES.KafkaProducer);
+  kafkaProducer.connect();
+
   api.listen(config.API_PORT, () =>
     console.log('The application is initialised on the port %s', config.API_PORT)
   );
--- packages/application/src/subscribers/index.ts
+++ packages/application/src/subscribers/index.ts
@@ -6,9 +6,19 @@ import 'reflect-metadata';
 import { initialise } from '../startup';
 import { TYPES } from '../types';
 import { IEventBus } from '@cqrs-es/core';
+import { Consumer } from 'kafkajs';
+import config from '@config/main';
 
 (async () => {
   const container = await initialise();
+  
+  const kafkaConsumer = container.get<Consumer>(TYPES.KafkaConsumer);
+  kafkaConsumer.connect();
+
+  for (const topic of config.KAFKA_TOPICS_TO_SUBSCRIBE.split(',')) {
+    await kafkaConsumer.subscribe({ topic });
+  }
+
   const baseEventHandler = container.get<IEventBus>(TYPES.EventBus);
   baseEventHandler.subscribeEvents();
 })();
